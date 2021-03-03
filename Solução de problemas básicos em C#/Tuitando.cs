12527f2b5a84e0bd477c52d6ae4e754e8b1ab87d
elaine16 <elaine.adm.redes@gmail.com>
2021-03-03 20:33:40 -0300

    Create Tuitando.cs

---
+++ Solução de problemas básicos em C#/Tuitando.cs
@@ -0,0 +1,40 @@
+/*
+ Tuitando
+
+O microblog Twitter é conhecido por limitar as postagens em 140 caracteres. Conferir se um texto vai caber em um tuíte é sua tarefa.
+
+Entrada
+A entrada é uma linha de texto T (1 ≤ |T| ≤ 500).
+
+Saída
+A saída é dada em uma única linha. Ela deve ser "TWEET" (sem as aspas) se a linha de texto T tem até 140 caracteres. Se T tem mais de 140 caracteres, a saída deve ser "MUTE".
+
+ 
+Exemplo de Entrada:
+RT @TheEllenShow: If only Bradley's arm was longer. Best photo ever. #oscars pic.twitter.com/C9U5NOtGap
+
+Exemplo de Saída:
+TWEET
+
+
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Uri_Awnsers
+{
+    class Program2165
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Console.WriteLine((text.Length > 140) ? "MUTE" : "TWEET");
+        }
+    }
+}
